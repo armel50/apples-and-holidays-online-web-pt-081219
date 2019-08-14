a76e68c5b33af64c8d63005d2b15152c4a829e03
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-14 07:31:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,9 +5,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  
   holiday_hash[:winter].each {|holyday, array_of_supply| array_of_supply << supply }
-
 end
 
 
