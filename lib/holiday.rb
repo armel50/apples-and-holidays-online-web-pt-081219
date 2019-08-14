0b661992bf5379038f39a2c513dc9fc898a32934
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-14 07:25:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ" 
   result = []
-   holiday_hash.each do |season, holiday|
+   holiday_hash.collect do |season, holiday|
       holiday.collect do |key, val|
        result << key if val.include?("BBQ")
      end
